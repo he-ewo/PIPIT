e1d923a74b9a6a58f98e06495c6f8837ffb7252b
he-ewo <munggchgg@gmail.com>
2024-08-22 23:57:20 +0900

    remove

---
+++ GAN/src/config/gan.py
@@ -0,0 +1,47 @@
+#epochs: int = 100
+epochs: int = 500
+
+d_loops: int = 2
+g_loops: int = 2
+
+d_lr: float = 1e-4
+g_lr: float = 1e-4
+
+#hl_lambda = 1e-5
+hl_lambda = 1e-4
+
+#wgan_clamp = [-0.1, 0.1]s
+wgan_clamp = [-0.05, 0.05]
+
+#wgangp_lambda = 10
+wgangp_lambda = 5
+
+
+
+
+
+
+#epochs: int = 300   
+
+#d_loops: int = 5    
+#g_loops: int = 1   
+#d_lr: float = 2e-4  
+#g_lr: float = 1e-4 
+#hl_lambda = 1e-5
+#wgan_clamp = [-0.1, 0.1]
+#wgangp_lambda = 10
+
+#epochs: int = 200  
+
+#d_loops: int = 500  
+#g_loops: int = 500  
+
+#d_lr: float = 1e-4  
+#g_lr: float = 5e-5  
+
+#hl_lambda = 5e-6  
+
+#wgan_clamp = [-0.05, 0.05] 
+
+#wgangp_lambda = 5  
+
